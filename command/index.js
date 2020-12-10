347868824dc164e32a9a2bbf95d427c34a2be292
zhaixiang <843476168@qq.com>
2020-12-10 12:02:21 +0800

    feat: 状态栏 默认左边 显示，可以配置成右边

--- command/index.js
+++ command/index.js
@@ -12,21 +12,25 @@ exports.registers = (context) => {
   const register = (commandName, cb) => {
     subscriptions.push(registerCommand(commandName, cb))
   }
-  const statusBar = new showStatusBar(context)
+  const statusBar = new showStatusBar(context, 'right')
   log.info('first register command...')
   // 下面是要注册的命令
   register('redis-stream.helloWorld', () => {
-    log.info('active command: helloWorld')
+    log.info('active command: helloWorld', 'error')
     let res = statusBar.text
-    statusBar.showMessage('heheh.....')
+    statusBar.showMessage('状态栏中文测试.....')
 
     showMsg('Hello World from redis-stream!')
+    showMsg('warning from redis-stream!', 'warning')
+    showMsg('error from redis-stream!', 'error')
   })
 
   register('redis-stream.hide', () => {
     showModal('要隐藏状态栏显示?', 'yes', 'no', 'or')
-      .then(res => console.log('res:', res))
-    statusBar.hideMessage()
+      .then(res => {
+        if (res === 'yes') statusBar.hideMessage()
+      })
+
   })
 
 
